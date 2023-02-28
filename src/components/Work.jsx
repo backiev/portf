8d5f1c5a8b816abaf1db57a1b5812cc7f0cb7de6
backiev <farkhat.bakiev@mail.ru>
2023-02-28 06:55:29 +0300

    texted block 'About me'

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Work = () => {
   return (
     <div className='my-6 container'>
-        <div className="title-block">Работа</div>
-        <p className="text-block indent-5">Takuya is a freelance and a full-stack developer based in Osaka with a passion for building digital services/stuff he wants. He has a knack for all things launching products, from planning and designing all the way to solving real-life problems with code. When not online, he loves hanging out with his camera. Currently, he is living off of his own product called Inkdrop. He publishes content for marketing his products and his YouTube channel called "Dev as Life" has more than 100k subscribers.</p>
+        <div className="title-block">О себе</div>
+        <p className="text-block indent-5">Обладаю высокой работоспособностью, умею работать в режиме многозадачности и в команде, имею развитые организационные и управленческие способности. Хобби: чтение книг, спорт. Занимался несколько лет веб-разработкой в виде хобби. Имею несколько пет-проектов (Ниже представлен список моих работ). Работаю в стеке: HTML + CSS + JS + REACT + REDUX.</p>
     </div>
   )
 }
