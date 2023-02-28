9b33d030929689361119172719cb24c49fa15ca3
backiev <farkhat.bakiev@mail.ru>
2023-02-28 07:07:06 +0300

    links images fixed

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -8,7 +8,7 @@ export const Title = ({currentTheme}) => {
         <div>Junior Frontend Developer</div>
         </div>
         <div className='h-auto w-32 '>
-          <img src="/images/me.jpg" alt="me" className='rounded-full border-white border-2 border-solid' style={{borderColor: currentTheme.borderColor}}/>
+          <img src="../images/me.jpg" alt="me" className='rounded-full border-white border-2 border-solid' style={{borderColor: currentTheme.borderColor}}/>
         </div>
     </div>
   )
