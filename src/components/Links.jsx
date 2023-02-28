6c16de40d424b227776d458cba3c86047d844a23
backiev <farkhat.bakiev@mail.ru>
2023-02-28 06:00:01 +0300

    added objects to links and projects

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faTelegram, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 export const Links = () => {
+
+  const links = [
+    {id: 0, name: 'Github', link: 'https://github.com/backiev', nickname: '@backiev', icon: <FontAwesomeIcon icon={faGithub} className="text-lg"/>},
+    {id: 1, name: 'Telegram', link: 'https://t.me/backiev', nickname: '@backiev', icon: <FontAwesomeIcon icon={faTelegram} className="text-lg"/> }
+  ];
+
   return (
     <div className='my-6 container' id="links">
         <div className="title-block mb-3 text-xl underline underline-offset-4 decoration-stone-200 decoration-4 font-bold">Ссылки</div>
         <div className='flex flex-col'>
-          <a href='https://github.com/backiev' target={'_blank'} className='text-cyan-400 hover:underline p-2 hover:bg-cyan-800/10 ease-in duration-200 rounded w-2/4 flex items-center'>
-            <FontAwesomeIcon icon={faGithub} className="text-lg"/> 
-            <span className='ml-2'>@backiev</span>
-          </a>
-          <a href='https://t.me/backiev' target={'_blank'} className='text-cyan-400 hover:underline p-2 hover:bg-cyan-800/10 ease-in duration-200 rounded w-2/4 flex items-center'>
-            <FontAwesomeIcon icon={faTelegram} className="text-lg"/> 
-            <span className='ml-2'>@backiev</span>
-          </a>
+          {links.map(item => (
+            <a href={item.link} target={'_blank'} className='text-cyan-400 hover:underline p-2 hover:bg-cyan-800/10 ease-in duration-200 rounded w-2/4 flex items-center'>
+              {item.icon}
+              <span className='ml-2'>{item.nickname}</span>
+            </a>
+          ))}
         </div>
     </div>
   )
